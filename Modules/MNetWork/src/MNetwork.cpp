0d2a44532b9dc1a0b3fc9fff8a663dd142fc0a58
arrobasaul <saul_v92@yahoo.es>
2022-09-19 17:54:07 -0600

    se agrega submodulo glm

--- Modules/MNetWork/src/MNetwork.cpp
+++ Modules/MNetWork/src/MNetwork.cpp
@@ -1,23 +1,23 @@
-#include <cstdlib>  /* Aquí contiene la función system("pause"); */
-#include <iostream> /* Importamos esta librería para hacer uso de cout y así ver el resultado en pantalla. */
-#include <string>
-#include <memory>
 #include "Base.h"
 #include "Layer.h"
 #include "imgui.h"
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
+#include <cstdlib>  /* Aquí contiene la función system("pause"); */
+#include <iostream> /* Importamos esta librería para hacer uso de cout y así ver el resultado en pantalla. */
+#include <memory>
+#include <string>
+/*#include <boost/beast/core.hpp>
 #include <boost/asio/strand.hpp>
+#include <boost/beast/http.hpp>
 #include <boost/beast/http/type_traits.hpp>
-#include <boost/json.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/json.hpp>*/
 #include <cstdlib>
 #include <functional>
 #include <iostream>
 #include <memory>
 #include <string>
 #include <thread>
-
+/*
 namespace beast = boost::beast; // from <boost/beast.hpp>
 namespace http = beast::http;   // from <boost/beast/http.hpp>
 namespace net = boost::asio;    // from <boost/asio.hpp>
@@ -25,7 +25,8 @@ namespace json = boost::json;
 using tcp = boost::asio::ip::tcp; // from <boost/asio/ip/tcp.hpp>
 using boost::json::value;
 //------------------------------------------------------------------------------
-
+*/
+/*
 // Report a failure
 void fail(beast::error_code ec, char const *what)
 {
@@ -71,7 +72,10 @@ public:
         value data = {
             {"email", "arrobasaul2@gmail.es"},
             {"password", "Olimpia3"}};
-        std::string message_to_client = std::string("{ \n\"email\": \"arrobasaul2@gmail.es\", \n\"password\": \"Olimpia3\" \n}"); // serialize(json::object{{"email", "arrobasaul2@gmail.es"}, {"password", "Olimpia3"}});
+        std::string message_to_client = std::string("{ \n\"email\":
+\"arrobasaul2@gmail.es\", \n\"password\": \"Olimpia3\" \n}"); //
+serialize(json::object{{"email", "arrobasaul2@gmail.es"}, {"password",
+"Olimpia3"}});
         // body = message_to_client;
         // req_.body() = message_to_client;
         req_.body() = serialize(data);
@@ -162,7 +166,7 @@ public:
         // If we get here then the connection is closed gracefully
     }
 };
-
+*/
 //------------------------------------------------------------------------------
 
 #ifndef _WIN32
@@ -170,124 +174,106 @@ public:
 #else
 #define EXPORT __declspec(dllexport)
 #endif
-extern "C" EXPORT std::string Name()
-{
-    char *nombre;
-    std::string str_obj("MNetwork");
-    nombre = &str_obj[0];
-    return str_obj;
+extern "C" EXPORT std::string Name() {
+  char *nombre;
+  std::string str_obj("MNetwork");
+  nombre = &str_obj[0];
+  return str_obj;
 }
 
-APP_MOD_INFO{
-    /* Name:            */ "MNetwork",
-    /* Description:     */ "My fancy new module",
-    /* Author:          */ "author1;author2,author3,etc...",
-    /* Version:         */ 0, 1, 0,
-    /* Max instances    */ -1};
+APP_MOD_INFO{/* Name:            */ "MNetwork",
+             /* Description:     */ "My fancy new module",
+             /* Author:          */ "author1;author2,author3,etc...",
+             /* Version:         */ 0,
+             1,
+             0,
+             /* Max instances    */ -1};
 
-class MNetwork : public AppSystem::Service
-{
+class MNetwork : public AppSystem::Service {
 public:
-    MNetwork(std::string name)
-    {
-        this->name = name;
-        // gui::menu.registerEntry(name, menuHandler, this, NULL);
+  MNetwork(std::string name) {
+    this->name = name;
+    // gui::menu.registerEntry(name, menuHandler, this, NULL);
+  }
+
+  ~MNetwork() {
+    // gui::menu.removeEntry(name);
+  }
+
+  void postInit() {}
+
+  void enable() { enabled = true; }
+
+  void disable() { enabled = false; }
+
+  bool isEnabled() { return enabled; }
+  virtual void OnUIRender() override {
+    ImGui::Begin("Hello from network");
+    static char _host[128];
+    static char _port[128];
+    static char _target[128];
+    static char _version[128];
+    ImGui::InputText("Host", _host, IM_ARRAYSIZE(_host));
+    ImGui::InputText("Port", _port, IM_ARRAYSIZE(_port));
+    ImGui::InputText("Target", _target, IM_ARRAYSIZE(_target));
+    ImGui::InputText("Version", _version, IM_ARRAYSIZE(_version));
+
+    if (ImGui::Button("Save")) {
+      auto const host = _host;
+      auto const port = _port;
+      auto const target = _target;
+      /*int v = !std::strcmp("1.0", _version) ? 10 : 11;
+
+      // The io_context is required for all I/O
+      net::io_context ioc;
+      auto start = std::chrono::system_clock::now();
+      // Your Code to Execute //
+      // Launch the asynchronous operation
+      std::make_shared<session>(ioc)->run(host, port, target, v);
+      auto end = std::chrono::system_clock::now();
+
+      std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end -
+                                                                         start)
+                       .count()
+                << "ms" << std::endl;
+      // Run the I/O service. The call will return when
+      // the get operation is complete.
+
+      // std::thread thercontext = std::thread([&]()
+      //                                  { ioc.run(); });
+*/
     }
-
-    ~MNetwork()
-    {
-        // gui::menu.removeEntry(name);
-    }
-
-    void postInit()
-    {
-    }
-
-    void enable()
-    {
-        enabled = true;
-    }
-
-    void disable()
-    {
-        enabled = false;
-    }
-
-    bool isEnabled()
-    {
-        return enabled;
-    }
-    virtual void OnUIRender() override
-    {
-        ImGui::Begin("Hello from network");
-        static char _host[128];
-        static char _port[128];
-        static char _target[128];
-        static char _version[128];
-        ImGui::InputText("Host", _host, IM_ARRAYSIZE(_host));
-        ImGui::InputText("Port", _port, IM_ARRAYSIZE(_port));
-        ImGui::InputText("Target", _target, IM_ARRAYSIZE(_target));
-        ImGui::InputText("Version", _version, IM_ARRAYSIZE(_version));
-
-        if (ImGui::Button("Save"))
-        {
-            auto const host = _host;
-            auto const port = _port;
-            auto const target = _target;
-            int v = !std::strcmp("1.0", _version) ? 10 : 11;
-
-            // The io_context is required for all I/O
-            net::io_context ioc;
-            auto start = std::chrono::system_clock::now();
-            // Your Code to Execute //
-            // Launch the asynchronous operation
-            std::make_shared<session>(ioc)->run(host, port, target, v);
-            auto end = std::chrono::system_clock::now();
-
-            std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-            // Run the I/O service. The call will return when
-            // the get operation is complete.
-
-            // std::thread thercontext = std::thread([&]()
-            //                                  { ioc.run(); });
-        }
-        ImGui::End();
-        // ImGui::ShowDemoWindow();
-    }
+    ImGui::End();
+    // ImGui::ShowDemoWindow();
+  }
 
 private:
-    static void menuHandler(void *ctx)
-    {
-        MNetwork *_this = (MNetwork *)ctx;
-        // ImGui::Text("Hello SDR++, my name is %s", _this->name.c_str());
-    }
+  static void menuHandler(void *ctx) {
+    MNetwork *_this = (MNetwork *)ctx;
+    // ImGui::Text("Hello SDR++, my name is %s", _this->name.c_str());
+  }
 
-    std::string name;
-    bool enabled = true;
+  std::string name;
+  bool enabled = true;
 };
 
-MOD_EXPORT void _INIT_()
-{
-    // Nothing here
-    printf("You are in inside Mcore2 on win\n");
+MOD_EXPORT void _INIT_() {
+  // Nothing here
+  printf("You are in inside Mcore2 on win\n");
 }
 
-MOD_EXPORT AppSystem::Service *_CREATE_INSTANCE_(std::string name)
-{
-    return new MNetwork(name);
+MOD_EXPORT AppSystem::Service *_CREATE_INSTANCE_(std::string name) {
+  return new MNetwork(name);
 }
 
-MOD_EXPORT void _DELETE_INSTANCE_(void *instance)
-{
-    delete (MNetwork *)instance;
+MOD_EXPORT void _DELETE_INSTANCE_(void *instance) {
+  delete (MNetwork *)instance;
 }
 
-MOD_EXPORT void _END_()
-{
-    // Nothing here
+MOD_EXPORT void _END_() {
+  // Nothing here
 }
-MOD_EXPORT AppSystem::Service *_CREATE_LAYER_(std::string name)
-{
-    printf("_CREATE_LAYER_\n");
-    return new MNetwork(name);
+MOD_EXPORT AppSystem::Service *_CREATE_LAYER_(std::string name) {
+  printf("_CREATE_LAYER_\n");
+  return new MNetwork(name);
 }
